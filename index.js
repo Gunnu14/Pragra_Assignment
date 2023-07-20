c29115042494c14677d1a2fe8dda89678bc5e2a3
Dhavneet <nitibilkhu@gmail.com>
2023-07-20 12:15:20 -0400

    added changes

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 <p id="demo">JavaScript can change HTML content.</p>
 
 <button type="button" onclick="document.getElementById('demo').innerHTML = 'Hello JavaScript!'">Click Me!</button>
+<button type="button" onclick="document.getElementById('demo').innerHTML = 'Hello Dhavneet!'">Click Me!</button>
+<button type="button" onclick="document.getElementById('demo').innerHTML = 'Hello Gunveer!'">Click Me!</button>
 
 </body>
 </html>
